dc47b753b524c7bd724d1a9810f3f0cf7007300b
vmeverington <vmeverington@gmail.com>
2023-08-01 14:17:29 -0400

    updated

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -23,7 +23,10 @@ class SudokuGenerator:
 	None
     '''
     def __init__(self, row_length, removed_cells):
-        pass
+        self.row_length = 9
+        self.removed_cells = removed_cells
+
+        self.box_length = int(row_length ** 0.5)
 
     '''
 	Returns a 2D python list of numbers which represents the board
@@ -32,7 +35,7 @@ class SudokuGenerator:
 	Return: list[list]
     '''
     def get_board(self):
-        pass
+        return self.board
 
     '''
 	Displays the board to the console
