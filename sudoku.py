d9f96cab7fe89f362f7538b2a108332de3c11da6
dghelerter <113321644+dghelerter@users.noreply.github.com>
2023-08-06 13:22:15 -0400

    Cell Class

--- sudoku.py
+++ sudoku.py
@@ -148,6 +148,20 @@ class Main:
                     if restart_rectangle.collidepoint(event.pos):
                         self.draw_start_screen()
 
+class Cell:
+    def __init__(self,value,row,col,screen):
+        self.value =  value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def set_cell_value(self,value):
+        self.value = value
+
+    def set_sketched_value(self,value):
+        self.value = value
+
+    def draw(self):
 
 if __name__ == '__main__':
     pygame.init()
